69280dd0fd5ec988f65c11395c6f092872afccd7
malaguna <malaguna@08bf6a96-f3ea-11de-91ba-91c70a792394>
2010-03-27 09:18:58 +0000

    #3, Done.

---
+++ src/es/urjc/mctwp/bbeans/research/image/DownloadBean.java
@@ -0,0 +1,127 @@
+package es.urjc.mctwp.bbeans.research.image;
+
+import java.io.FileInputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletResponse;
+
+import es.urjc.mctwp.bbeans.RequestInvAbstractBean;
+import es.urjc.mctwp.image.objects.Image;
+import es.urjc.mctwp.image.objects.SeriesImage;
+import es.urjc.mctwp.image.objects.SingleImage;
+import es.urjc.mctwp.modelo.Trial;
+import es.urjc.mctwp.service.commands.imageCmds.LoadImage;
+
+public class DownloadBean extends RequestInvAbstractBean {
+	private int BUFFER_SIZE = 4096;
+	
+	public String accDownloadImage(){
+		String imageId = getSession().getImage().getImageId();
+		
+		if(imageId != null && !imageId.isEmpty()){
+			Trial trial = getSession().getTrial();
+			
+			try{
+				if(trial != null){
+					FacesContext ctx = FacesContext.getCurrentInstance();  
+					HttpServletResponse response = (HttpServletResponse) ctx.getExternalContext().getResponse();  
+	
+					LoadImage cmd = (LoadImage) getCommand(LoadImage.class);
+					cmd.setCollection(trial.getCollection());
+					cmd.setImageId(imageId);
+					cmd.setUser(getSession().getUser());
+					cmd.setTrial(trial);
+					cmd = (LoadImage)runCommand(cmd);
+					Image img = cmd.getResult();
+	
+					//Avoid caching image files is response
+					response.setHeader("pragma", "no-cache");
+					response.setHeader("Cache-control", "no-cache, no-store, must-revalidate");
+					response.setHeader("Expires", "01 Jan 2000 00:00:00 GMT");
+	
+					if(img instanceof SingleImage){
+						SingleImage single = (SingleImage)img;
+						
+						//If the file exists
+						if(single.getContent().exists()){
+				
+							//Get file name without parents
+							String contentType = img.getType();
+							String fileName = imageId + "." + contentType;
+							
+							//Prepare header
+							response.setContentType(contentType);
+							StringBuffer contentDisposition = new StringBuffer();
+							contentDisposition.append("attachment;");
+							contentDisposition.append("filename=\"");
+							contentDisposition.append(fileName);
+							contentDisposition.append("\"");
+							response.setHeader ("Content-Disposition", contentDisposition.toString());
+					
+							//Put file content
+							byte bytes[] = new byte[BUFFER_SIZE];
+							FileInputStream fileIS = new FileInputStream(single.getContent());
+							while(fileIS.read(bytes) != -1)
+								response.getOutputStream().write(bytes);
+							
+							fileIS.close();
+						}
+					}else{
+						SeriesImage serie = (SeriesImage)img;
+						
+						//Get file name without parents
+						String contentType = img.getType();
+						String fileName = imageId + ".zip";
+						
+						//Prepare header
+						response.setContentType(contentType);
+						StringBuffer contentDisposition = new StringBuffer();
+						contentDisposition.append("attachment;");
+						contentDisposition.append("filename=\"");
+						contentDisposition.append(fileName);
+						contentDisposition.append("\"");
+						response.setHeader ("Content-Disposition", contentDisposition.toString());
+						
+						if(serie.getImages() != null){
+							//Prepare ZipOutputStream and writes files into
+							ZipOutputStream zos;
+							zos = new ZipOutputStream(response.getOutputStream());
+							
+							//Add every image to zip file
+							for(Image i : serie.getImages()){
+	
+								if(i instanceof SingleImage){
+									SingleImage si = (SingleImage)i;
+									
+									//Create zip entry
+									ZipEntry ze = new ZipEntry(si.getContent().getName());
+									zos.putNextEntry(ze);
+
+									//Write zip entry
+									byte bytes[] = new byte[BUFFER_SIZE];
+									FileInputStream fileIS = new FileInputStream(si.getContent());;
+									while(fileIS.read(bytes) != -1)
+										zos.write(bytes);
+									
+									fileIS.close();
+								}					
+							}
+							
+							//Close zip file
+							zos.close();
+						}
+					}
+					
+					ctx.responseComplete();
+				}
+			} catch (Exception e) {
+				setErrorMessage(e.getLocalizedMessage());
+				e.printStackTrace();
+			}
+		}
+				
+		return null;
+	}
+}
